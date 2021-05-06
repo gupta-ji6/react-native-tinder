9bca0744146577da3ddcfc6d605955a4a6c079ee
gupta-ji6 <ayushgupta197@gmail.com>
2021-05-06 11:59:46 +0530

    :heavy_plus_sign: Add @react-navigation/bottom-tabs

--- App.js
+++ App.js
@@ -5,18 +5,21 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import Footer from './src/components/Footer';
 import TinderCards from './src/components/TinderCards';
 
 function App() {
   return (
-    <View style={styles.container}>
-      <TinderCards />
-      <Footer />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <TinderCards />
+        <Footer />
+      </View>
+    </NavigationContainer>
   );
 }
 
